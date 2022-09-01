d06474ae5cf0d6b0575c013126d946c3e7c45bb0
Nomael <45855919+Nomael@users.noreply.github.com>
2022-09-01 08:01:29 +0200

    Update

--- Schule/Rechner Noe Mal/Rechner Noe Mal/Properties/AssemblyInfo.cs
+++ Schule/Rechner Noe Mal/Rechner Noe Mal/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die einer Assembly zugeordnet sind.
-[assembly: AssemblyTitle("Rechner Noe Mal")]
+[assembly: AssemblyTitle("Rechner_Noel_Malchow")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rechner Noe Mal")]
+[assembly: AssemblyProduct("Rechner_Noel_Malchow")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
