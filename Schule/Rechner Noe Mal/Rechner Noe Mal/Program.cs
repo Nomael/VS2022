3ad6ac5b811397cf7cbdaffa9b21e126faf1fe80
Nomael <45855919+Nomael@users.noreply.github.com>
2022-09-01 07:57:25 +0200

    Einfache Rechner Aufgabe

---
+++ Schule/Rechner Noe Mal/Rechner Noe Mal/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rechner_Noe_Mal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int zahl1;
+            int zahl2;
+            int ergebniss;
+
+            Console.Write("Zahl 1: ");
+            zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Zahl 2: ");
+            zahl2 = Convert.ToInt32(Console.ReadLine());
+
+
+            ergebniss = zahl1 + zahl2;
+
+            Console.WriteLine(zahl1 + " + " + zahl2 + " = " + ergebniss);
+
+            Console.ReadKey();
+        }
+    }
+}
