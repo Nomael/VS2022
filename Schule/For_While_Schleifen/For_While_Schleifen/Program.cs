8aadb2360b77ef254c24c5a6a58b3326847e9417
Nomael <45855919+Nomael@users.noreply.github.com>
2022-09-08 09:14:40 +0200

    Update

---
+++ Schule/For_While_Schleifen/For_While_Schleifen/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_While_Schleifen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            for (i = 1; i <= 10; i += (1))
+            {
+                Console.Write("For Schleife: "); Console.WriteLine(i);
+            }
+            Console.WriteLine("------------------------");
+            i = 0;
+            while (i < 10)
+            {
+                i += 1;
+                Console.Write("While Schleife: "); Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
