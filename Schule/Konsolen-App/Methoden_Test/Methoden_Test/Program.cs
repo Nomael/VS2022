001c9b70fe7837b613ef18d8f0fd78cdce3a03cb
Nomael <45855919+Nomael@users.noreply.github.com>
2022-09-23 12:28:17 +0200

    Upload

---
+++ Schule/Konsolen-App/Methoden_Test/Methoden_Test/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methoden_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SchleifenLaenge(); // Ohne Rückgabe, Ohne Parameter
+            //SchleifenIntLaenge(20); // Ohne Rückgabe, Mit Parameter
+            //Console.WriteLine("Quadrat von 2 ist " + RechneQuadrat(2)); // Mit Rückgabe, Mit Parameter
+
+            Console.WriteLine(ZeichenRate(4));
+
+            Console.ReadKey();
+        }
+
+        // Ohne Rückgabe, Ohne Parameter
+        static void SchleifenLaenge()
+        {
+
+        }
+
+        // Ohne Rückgabe, Mit Parameter
+        /// <summary> Länge des Quadrates </summary> <param name="plaenge"></param>
+        static void SchleifenIntLaenge(int plaenge)
+        {
+            for (int i = 0; i < plaenge; i++)
+            {
+                Console.Write("\n");
+                for (int j = 0; j < plaenge; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("#");
+            }
+        }
+
+        // Mit Rückgabe, Mit Parameter
+        static double RechneQuadrat(double pZahl)
+        {
+
+            double result = pZahl * pZahl;
+            return result;
+        }
+
+        static string ZeichenRate(int pZeichenLaenge)
+        {
+            string result = "";
+
+            for (int i = 0; i < pZeichenLaenge; i++)
+            {
+                result += "#";
+            }
+
+            return result;
+        }
+    }
+}
