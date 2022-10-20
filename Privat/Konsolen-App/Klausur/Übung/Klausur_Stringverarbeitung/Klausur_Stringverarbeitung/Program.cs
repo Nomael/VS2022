4511486b4fb48d9d09f990b22b28c87ada2bd1e3
Nomael <45855919+Nomael@users.noreply.github.com>
2022-10-20 18:27:02 +0200

    Update

---
+++ Privat/Konsolen-App/Klausur/Übung/Klausur_Stringverarbeitung/Klausur_Stringverarbeitung/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klausur_Stringverarbeitung
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberArray = new string[] { "Hallo","werr", "Hallo", "werr", "Hallo", "werr", "Hallo", "werr" };
+
+            foreach (char nummer in numberArray)
+            {
+                Console.WriteLine(nummer);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
