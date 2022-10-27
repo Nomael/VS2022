b01d1535f32652902966db212fa88bbfce0da4cc
Nomael <45855919+Nomael@users.noreply.github.com>
2022-10-27 09:12:45 +0200

    Update

---
+++ Schule/Konsolen-App/KLAUSUR/Malchow-K1/Malchow-K1/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Malchow_K1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //------------------------------------------------------------
+            // Name:			ZeichenProgramm
+            // 
+            // Funktion:    Zeichnet verschiedene Formen auf die Konsole
+            // -----------------------------------------------------------
+            // Parameter: keine
+            // ------------------------------------------------------------
+            // 21.10.2022  1.Version
+            // ------------------------------------------------------------
+
+
+            string eingabe;
+
+			do
+			{
+				Console.Clear();
+				Console.WriteLine("Zeichenprogramm (by Noel Malchow)");
+				Console.WriteLine("---------------------");
+				Console.WriteLine("Bitte gebe eine Aufgabe ein");
+				Console.WriteLine("(a) Zeichne ein Quadrat");
+				Console.WriteLine("(b) Zeichne ein Rechteck");
+				Console.WriteLine("(c) Zeichne ein Dreieck");
+                Console.WriteLine("(d) Zeichne eine Linie");
+                Console.WriteLine("---------------------");
+				Console.WriteLine("(end) Ende");
+
+				Console.Write("\nAufgabe: ");
+				eingabe = Console.ReadLine();
+
+				switch (eingabe)
+				{
+					case "a":
+						ZeichneQuadrat();
+                        break;
+					case "b":
+						int zrbreite;
+						int zrhöhe;
+
+						zrbreite = Convert.ToInt32(Console.ReadLine());
+                        zrhöhe = Convert.ToInt32(Console.ReadLine());
+
+                        ZeichneRechteck(zrbreite,zrhöhe);
+						break;
+
+					case "c":
+						int zdbreite;
+
+                        zdbreite = Convert.ToInt32(Console.ReadLine());
+
+                        ZeichneDreieck(zdbreite);
+						break;
+
+                }
+
+				if (eingabe != "end")
+				{
+					Console.ReadKey();
+				}
+
+			} while (eingabe != "end");
+		}
+
+		static void ZeichneQuadrat()
+		{
+			int zqgröße = 9;
+
+			for (int i = 0; i < zqgröße; i++)
+			{
+				for (int j = 0; j < zqgröße; j++)
+				{
+					Console.Write("##");
+				}
+				Console.Write("\n");
+			}
+		}
+
+		static void ZeichneRechteck(int pbreite, int phöhe)
+		{
+            for (int i = 0; i < phöhe; i++)
+            {
+                for (int j = 0; j < pbreite; j++)
+                {
+                    Console.Write("#");
+                }
+                Console.Write("\n");
+            }
+        }
+
+		static void ZeichneDreieck(int pbreite)
+		{
+            for (int zdzeile = 1; zdzeile <= pbreite; zdzeile++)
+            {
+                for (int zdspalte = 1; zdspalte <= zdzeile; zdspalte++)
+                {
+                    Console.Write("#");
+                }
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Schule/Konsolen-App/Moving_Line/Moving_Line/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Moving_Line
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int xpos = 0;
+            int ypos = 0;
+            int laenge = 0;
+
+            ypos = Convert.ToInt32(Console.ReadLine());
+            xpos = Convert.ToInt32(Console.ReadLine());
+            laenge = Convert.ToInt32(Console.ReadLine());
+
+            int laengemin = 100;
+
+            //moving_loader(ypos, laenge, laengemin);
+            moving_line(ypos, xpos, laenge);
+
+
+            Console.ReadKey();
+        }
+
+        static void moving_loader(int pypos, int plaenge, int plaengeminus)
+        {
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < pypos; i++)
+                {
+                    Console.Write("\n");
+                }
+
+                Console.Write("Downloading[");
+                for (int i = 0; i < plaenge; i++)
+                {
+                    Console.Write("=");
+                }
+
+                Console.Write(">]" + plaenge);
+
+                Thread.Sleep(500);
+                if (plaengeminus != 0)
+                {
+                    plaengeminus--;
+                    plaenge++;
+                }
+
+            } while (plaengeminus != 0);
+        }
+
+        static void moving_line(int pypos, int pxpos, int plaenge)
+        {
+            do
+            {
+                Console.Clear();
+                for (int i = 0; i < pypos; i++)
+                {
+                    Console.Write("\n");
+                }
+
+                for (int i = 0; i < pxpos; i++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int i = 0; i < plaenge; i++)
+                {
+                    Console.Write("#");
+                }
+
+                Thread.Sleep(500);
+                pxpos++;
+
+            } while (true);
+        }
+    }
+}
