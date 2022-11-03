4350be6632608e2f106f4750f61accb3e13477c1
Nomael <45855919+Nomael@users.noreply.github.com>
2022-11-03 09:15:38 +0100

    Update

---
+++ Schule/Windows Forms-App/Steuerelement_Progressbar/Steuerelement_Progressbar/Form1.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace Steuerelement_Progressbar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pgb_display = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // pgb_display
+            // 
+            this.pgb_display.Location = new System.Drawing.Point(220, 159);
+            this.pgb_display.Name = "pgb_display";
+            this.pgb_display.Size = new System.Drawing.Size(338, 107);
+            this.pgb_display.TabIndex = 0;
+            this.pgb_display.Click += new System.EventHandler(this.pgb_display_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pgb_display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar pgb_display;
+    }
+}
+
---
+++ Schule/Windows Forms-App/Steuerelement_Progressbar/Steuerelement_Progressbar/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Steuerelement_Progressbar
+{
+    public partial class Form1 : Form
+    {
+        double txt = 40;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pgb_display_Click(object sender, EventArgs e)
+        {
+            // Display the ProgressBar control.
+            pgb_display.Visible = true;
+            // Set Minimum to 1 to represent the first file being copied.
+            pgb_display.Minimum = 1;
+            // Set Maximum to the total number of files to copy.
+            pgb_display.Maximum = Convert.ToInt32(txt);
+            // Set the initial value of the ProgressBar.
+            pgb_display.Value = 1;
+            // Set the Step property to a value of 1 to represent each file being copied.
+            pgb_display.Step = 1;
+
+            // Loop through all files to copy.
+            for (double x = 1; x <= txt; x += 0.01)
+            {
+                // Perform the increment on the ProgressBar.
+                Task.Delay(2000).Wait();
+                pgb_display.PerformStep();
+            }
+        }
+    }
+}
